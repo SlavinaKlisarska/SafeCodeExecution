93d4ac5198646a0d230cea7a9faa6221c3c47dcc
Slavina Klisarska <slavina_klisarska@epam.com>
2020-12-14 14:07:41 +0200

    Adding Get Request for the Repo

--- src/main/java/input/RequestReceiver.java
+++ src/main/java/input/RequestReceiver.java
@@ -2,8 +2,11 @@ package input;
 
 import execution.RequestExecutionQueueHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +15,17 @@ public class RequestReceiver {
     @Autowired
     private RequestExecutionQueueHolder requestExecutionQueue;
 
+    @Value( "${repo.url}" )
+    private String repoUrl;
+
+    @GetMapping(value="/repoUrl")
+    @ResponseBody
+    public String getRepo() {
+        return repoUrl;
+    }
+
     @PostMapping(path = "/acceptTask", consumes = "application/json", produces = "application/json")
+    @ResponseBody
     public int acceptCodeTask(@RequestParam(value = "userEmail") String userEmail, @RequestParam(value = "task") String task, @RequestParam(value = "code") String code) {
 
         requestExecutionQueue.addRequest(new Request(task, code));
